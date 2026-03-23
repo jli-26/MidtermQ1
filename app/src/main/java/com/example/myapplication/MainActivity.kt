9615082381fa992fcd6833ace6e9a2d2c6674fde
jenjenli <jli26@bu.edu>
2026-03-23 13:48:50 -0400

    simple screen

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.saveable.rememberSaveable
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyApplicationTheme {
+                    CounterScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun CounterScreen() {
+
+    var count by rememberSaveable { mutableStateOf(0) }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = count.toString(),
+            fontSize = 40.sp
+        )
+        Row {
+            Button(
+                onClick = { count++ }
+            ) {
+                Text("Increment")
+            }
+            Button(
+                onClick = { count = 0 }
+            ) {
+                Text("Reset")
+            }
+        }
+    }
+}
